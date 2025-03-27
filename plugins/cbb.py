75d289cc21d9bd3deba384f9ffa96cdefe8ae5de
Mr Ghost <s.rytgam.ing89@gmail.com>
2025-03-27 21:39:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -7,9 +7,12 @@ from database.database import add_user, del_user, full_userbase, present_user
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+    user = query.from_user
+    mention = user.mention if user.mention else user.first_name
+    
     if data == "help":
         await query.message.edit_text(
-            text=HELP_TXT.format(first=query.from_user.first_name),
+            text=HELP_TXT.format(first=user.first_name, mention=mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -22,7 +25,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            text=ABOUT_TXT.format(first=user.first_name, mention=mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -33,7 +36,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "start":
         await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
+            text=START_MSG.format(first=user.first_name, mention=mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
